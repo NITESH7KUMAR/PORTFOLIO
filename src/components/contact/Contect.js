05eb17a2be3b61edebb463a66e04c6a09196dcdf
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2024-12-21 19:36:18 +0530

    begining

---
+++ src/components/contact/Contect.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home(){
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <Link to="/experience">Go to About Page</Link>
+        </div>
+    )
+}
+export default Home;
