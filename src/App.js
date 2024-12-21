a27799b72ec0234a81262621bd123ea82d9fb302
NITESH KUMAR <119055912+NITESH7KUMAR@users.noreply.github.com>
2024-12-21 20:04:33 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/home/Home.js';
+import Contact from './components/contact/Contact.js';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes> 
-          <Route path="/" element={<Home />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />  
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Home(){
     return(
         <div>
-            <h1>Home Page</h1>
+            <h1>Hom Page</h1>
             <Link to="/contact">Go to About Page</Link>
         </div>
     )
